0e929732340e39f1f56882e0804089c09193ae3f
nopri888-anto <79070725+nopri888-anto@users.noreply.github.com>
2022-01-29 22:00:11 +0700

    Update

--- index.php
+++ index.php
@@ -1,22 +1,144 @@
 <!DOCTYPE html>
 <html>
 
-<head>
 
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Task-tinymce</title>
+
+    <script src="tinymce/js/tinymce/tinymce.min.js"></script>
     <script>
+        var useDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
         tinymce.init({
-            selector: 'textarea'
+            selector: 'textarea',
+            plugins: 'print preview powerpaste casechange importcss tinydrive searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker imagetools textpattern noneditable help formatpainter permanentpen pageembed charmap tinycomments mentions quickbars linkchecker emoticons advtable export',
+            tinydrive_token_provider: 'URL_TO_YOUR_TOKEN_PROVIDER',
+            tinydrive_dropbox_app_key: 'YOUR_DROPBOX_APP_KEY',
+            tinydrive_google_drive_key: 'YOUR_GOOGLE_DRIVE_KEY',
+            tinydrive_google_drive_client_id: 'YOUR_GOOGLE_DRIVE_CLIENT_ID',
+            mobile: {
+                plugins: 'print preview powerpaste casechange importcss tinydrive searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker textpattern noneditable help formatpainter pageembed charmap mentions quickbars linkchecker emoticons advtable'
+            },
+            menu: {
+                tc: {
+                    title: 'Comments',
+                    items: 'addcomment showcomments deleteallconversations'
+                }
+            },
+            menubar: 'file edit view insert format tools table tc help',
+            toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment',
+            autosave_ask_before_unload: true,
+            autosave_interval: '30s',
+            autosave_prefix: '{path}{query}-{id}-',
+            autosave_restore_when_empty: false,
+            autosave_retention: '2m',
+            image_advtab: true,
+            link_list: [{
+                    title: 'My page 1',
+                    value: 'https://www.tiny.cloud'
+                },
+                {
+                    title: 'My page 2',
+                    value: 'http://www.moxiecode.com'
+                }
+            ],
+            image_list: [{
+                    title: 'My page 1',
+                    value: 'https://www.tiny.cloud'
+                },
+                {
+                    title: 'My page 2',
+                    value: 'http://www.moxiecode.com'
+                }
+            ],
+            image_class_list: [{
+                    title: 'None',
+                    value: ''
+                },
+                {
+                    title: 'Some class',
+                    value: 'class-name'
+                }
+            ],
+            importcss_append: true,
+            templates: [{
+                    title: 'New Table',
+                    description: 'creates a new table',
+                    content: '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>'
+                },
+                {
+                    title: 'Starting my story',
+                    description: 'A cure for writers block',
+                    content: 'Once upon a time...'
+                },
+                {
+                    title: 'New list with dates',
+                    description: 'New List with dates',
+                    content: '<div class="mceTmpl"><span class="cdate">cdate</span><br /><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>'
+                }
+            ],
+            template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
+            template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
+            height: 300,
+            image_caption: true,
+            quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+            noneditable_noneditable_class: 'mceNonEditable',
+            toolbar_mode: 'sliding',
+            spellchecker_ignore_list: ['Ephox', 'Moxiecode'],
+            tinycomments_mode: 'embedded',
+            content_style: '.mymention{ color: gray; }',
+            contextmenu: 'link image imagetools table configurepermanentpen',
+            a11y_advanced_options: true,
+            skin: useDarkMode ? 'oxide-dark' : 'oxide',
+            content_css: useDarkMode ? 'dark' : 'default',
+            /*
+            The following settings require more configuration than shown here.
+            For information on configuring the mentions plugin, see:
+            https://www.tiny.cloud/docs/plugins/premium/mentions/.
+            */
+            // mentions_selector: '.mymention',
+            // mentions_fetch: mentions_fetch,
+            // mentions_menu_hover: mentions_menu_hover,
+            // mentions_menu_complete: mentions_menu_complete,
+            // mentions_select: mentions_select,
+            // mentions_item_type: 'profile'
         });
     </script>
+
 </head>
 
 <body>
-    <h1>Add Post</h1>
 
-    <textarea>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-2"></div>
+            <div class="col-sm">
+                <h1 class="text-center">Add New Post to Wordpress below</h1>
+                <form>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="textInputPostSubjectArticle" name="textInputPostSubjectArticle" placeholder="Subject Post Article">
+                    </div>
+                    <div class="form-group">
+                        <textarea></textarea>
+                    </div>
+                    <button type="button" class="btn btn-primary">Publish</button>
+                </form>
 
-    </textarea>
+            </div>
+            <div class="col-sm-2"> </div>
+        </div>
+    </div>
+
 </body>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="js/bootstrap.min.js"></script>
 
 </html>
