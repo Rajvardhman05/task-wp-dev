e03ebc06b9de0402e7c2bd3731fb4ac8979f8e92
nopri888-anto <79070725+nopri888-anto@users.noreply.github.com>
2022-01-29 21:35:58 +0700

    Update

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <script>
+        tinymce.init({
+            selector: 'textarea'
+        });
+    </script>
+</head>
+
+<body>
+    <h1>Add Post</h1>
+
+    <textarea>
+
+    </textarea>
+</body>
+
+</html>
